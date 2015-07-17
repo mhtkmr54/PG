7fd452a40f203a84e68cfd38758fa18945bac559
mhtkmr54 <mhtkmr54@users.noreply.github.com>
2015-07-17 13:00:32 +0530

    Updated salt

--- failure.php
+++ failure.php
@@ -8,7 +8,7 @@ $posted_hash=$_POST["hash"];
 $key=$_POST["key"];
 $productinfo=$_POST["productinfo"];
 $email=$_POST["email"];
-$salt="GQs7yium";
+$salt="zKDU8nAp";
 
 If (isset($_POST["additionalCharges"])) {
        $additionalCharges=$_POST["additionalCharges"];
